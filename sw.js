51c65a69d2af2a9d82c132d935164367d3509e23
ryantrisyardi-glitch <ryan.trisyardi@gmail.com>
2026-08-14 08:11:58 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
 // Naikkan angka versi ini setiap kali ada perubahan pada app shell
 // supaya service worker lama otomatis dibuang dan cache baru dibuat.
-const SW_VERSION = 'v12';
+const SW_VERSION = 'v13';
 const CACHE_NAME = 'matika-app-shell-' + SW_VERSION;
 const APP_SHELL = [
   './',
